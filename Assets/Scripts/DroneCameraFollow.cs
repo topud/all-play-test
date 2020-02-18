38eea29f0830d26738d11ef18e389ae0238cd95f
devops-star <59298274+devops-star@users.noreply.github.com>
2020-02-18 12:26:43 -0500

    Drone Followme

---
+++ Assets/Scripts/DroneCameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCameraFollow : MonoBehaviour
+{
+    public GameObject ARCamera;
+    public float delay = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var rotation = Quaternion.LookRotation(ARCamera.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * delay);
+
+        //Move drone to camera, not working yet
+        //Vector3 newTargetPos = ARCamera.transform.position;
+        //newTargetPos.z = transform.position.z;
+        //newTargetPos.x = transform.position.x;
+
+        //transform.position = newTargetPos ;
+    }
+}

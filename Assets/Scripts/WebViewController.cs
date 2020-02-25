ae5e4e52d81aec82d39ff407c204fc61d4854856
devops-star <59298274+devops-star@users.noreply.github.com>
2020-02-25 00:03:23 -0500

    Implemented print functionality

---
+++ Assets/Scripts/WebViewController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[RequireComponent(typeof(UnityEngine.UI.Button))]
+public class WebViewController : MonoBehaviour
+{
+	UniWebView webView;
+
+	public void Print()
+	{
+		var webViewGameObject = new GameObject("UniWebView");
+		webView = webViewGameObject.AddComponent<UniWebView>();
+
+		webView.Frame = new Rect(Screen.width, Screen.height, Screen.width, Screen.height);
+		//webView.Frame = new Rect(0, 0, Screen.width, Screen.height);
+		string url = UniWebViewHelper.StreamingAssetURLForPath("html/printSamplePortrait.html");
+
+		webView.Load(url);
+		webView.Show();
+
+		webView.OnPageFinished += (view, statusCode, loadedUrl) =>
+		{
+			webView.Print();
+		};
+
+		webView.OnMessageReceived += (view, message) =>
+		{
+			if (message.Path == "close")
+			{
+				webView.Hide();
+			}
+		};
+	}
+
+}

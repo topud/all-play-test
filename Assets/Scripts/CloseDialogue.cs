424084811e43d7dba647e127f1eee96319d45689
Bryan Aument <bryan.aument@ey.com>
2020-02-26 18:01:18 -0500

    closed dialogue when tap on dismiss

---
+++ Assets/Scripts/CloseDialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDialogue : MonoBehaviour
+{
+    public GameObject modalDialogue;
+    public void close() {
+        modalDialogue.SetActive(false);
+    }
+}

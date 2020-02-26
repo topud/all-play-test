44e81704c327c15c107d4116f4154583727e38f0
Bryan Aument <bryan.aument@ey.com>
2020-02-26 16:49:45 -0500

    added pagination with Aki

--- Assets/Scripts/DialogueButtons.cs
+++ Assets/Scripts/DialogueButtons.cs
@@ -4,35 +4,40 @@ using UnityEngine;
 
 public class DialogueButtons : MonoBehaviour
 {
+    private int diagPointer = 0; 
     // Start is called before the first frame update
     List<Component> contentSection = new List<Component>();
     void Start()
     {
         // gather all children of the button's parent i.e. siblings
-        Component[] items = gameObject.transform.parent.GetComponentsInChildren<RectTransform>();
+        Component[] items = gameObject.transform.GetComponentsInChildren<RectTransform>();
 
-        foreach(Component item in items) {
-            Debug.LogFormat("I AM TYPE:  {0}", item.GetType());
-            contentSection.Add(item);
+        foreach(Component item in items) {     
+            if(item.tag == "DialogueText"){
+                contentSection.Add(item);
+                item.gameObject.SetActive(false);
+            }
         }
-        foreach(Component item in contentSection) {
-            Debug.LogFormat("Some items that are in the list: {0}", item);
-        }
-        // filter siblings to only text to display into List
-        // foreach (Component item in items) {
-        //    if(item.tag == "DialogueText") {
-        //        contentSection.Add(item);
-        //        Debug.LogFormat("Added", item);
-        //    } else {
-        //        Debug.LogFormat("NOT ADDED!!!!!!!!");
-        //        Debug.LogFormat(item.tag);
-        //    }
-        // }
-
+        // making first item in list viewable
+        contentSection[0].gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    public void navigateForward() {
+        contentSection[diagPointer].gameObject.SetActive(false);
+        if(contentSection.Count - 1 == diagPointer) {
+            diagPointer = 0;
+        } else {
+            diagPointer++;
+        }
+        contentSection[diagPointer].gameObject.SetActive(true);
+    }
+
+    public void navigateBackward() {
+        diagPointer--;
     }
 }
---
+++ Assets/Scripts/nextButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nextButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform parentDialogue;
+    public GameObject dialogue;
+    public void next() {
+        dialogue.GetComponent<DialogueButtons>().navigateForward();
+    }
+}

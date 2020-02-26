78fcb63e447facd06624f09ca0bdca61ff35eb52
Bryan Aument <bryan.aument@ey.com>
2020-02-26 14:11:48 -0500

    update to scripts and pagination

--- Assets/Scripts/DialogueButtons.cs
+++ Assets/Scripts/DialogueButtons.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class DialogueButtons : MonoBehaviour
 {
     // Start is called before the first frame update
+    List<Component> contentSection;
     void Start()
     {
-        
+        // gather all children of the button's parent i.e. siblings
+        Component[] items = gameObject.transform.parent.GetComponentsInChildren<Transform>();
+        // filter siblings to only text to display into List
+        foreach (Component item in items) {
+           if(item.tag == "DialogeText") {
+               contentSection.Add(item);
+               Debug.LogFormat("Added", item);
+           }
+           
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }

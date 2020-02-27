9b91ddb9654ac9d700c2832ec9e8c9534326727c
Cole Reed <cole.reed@ey.com>
2020-02-27 12:26:18 -0800

    feat(placemat-side-a): barricade and sign

---
+++ Assets/Scripts/BarracadeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarracadeController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject lightFlairs;
+
+    // More Animation TBD
+    //[SerializeField]
+    //private GameObject lightFlair01;
+
+    //[SerializeField]
+    //private GameObject lightFlair02;
+
+
+    private bool isPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play()
+    {
+        isPlaying = true;
+        lightFlairs.SetActive(isPlaying);
+    }
+
+    public void stop()
+    {
+        isPlaying = false;
+        lightFlairs.SetActive(isPlaying);
+    }
+
+    public void toggleAni()
+    {
+        if(isPlaying)
+        {
+            stop();
+        } else
+        {
+            play();
+        }
+
+    }
+}

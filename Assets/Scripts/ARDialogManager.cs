05a15ad79592b975f65d763d81555217b3d86733
devops-star <59298274+devops-star@users.noreply.github.com>
2020-02-27 14:42:58 -0500

    Callouts wiring

---
+++ Assets/Scripts/ARDialogManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARDialogManager : MonoBehaviour
+{
+    public Dictionary<string, GameObject> dictDialogs = new Dictionary<string, GameObject>();
+    public string dialogTag = "DialogueText";
+    
+    private void Start()
+    {
+        
+        //dictDialogs.Add(test1.name, test1);
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            Transform child = transform.GetChild(0).GetChild(i);
+            if (child.tag == dialogTag)
+            {
+                dictDialogs.Add(child.name, child.gameObject);
+                //Debug.Log("adding " + child.name);
+            }
+          
+        }
+        hideAllDialogs();
+
+    }
+
+
+    //Accepts a string as dialog and makes it visible
+    public void showDialog(string dialogName)
+    {
+        hideAllDialogs();
+        dictDialogs[dialogName].SetActive(true);
+    }
+
+    //Hides all dialogs shown.
+    public void hideAllDialogs()
+    {
+        foreach (var gameo in dictDialogs.Values)
+            gameo.SetActive(false);
+    }
+}

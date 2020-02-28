d4f3f438cbfc0064f46b7b0eefa4dcc0a80fb2db
Cole Reed <cole.reed@ey.com>
2020-02-27 17:04:46 -0800

    feat(placemat-side-b): add laptop prefab and animation script

---
+++ Assets/Scripts/LaptopController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LaptopController : MonoBehaviour
+
+{
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private bool playOnStart = false;
+
+    private bool isPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playOnStart)
+        {
+            play();
+        }
+        else
+        {
+            stop();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void play()
+    {
+        isPlaying = true;
+        animator.SetBool("play", isPlaying);
+    }
+
+    public void stop()
+    {
+        isPlaying = false;
+        animator.SetBool("play", isPlaying);
+    }
+
+    public void toggleAni()
+    {
+        if (isPlaying)
+        {
+            stop();
+        }
+        else
+        {
+            play();
+        }
+
+    }
+}
